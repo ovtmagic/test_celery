f4c5a9b4dea377dadc200588864f52b795e33e9e
Octavio Vicente <ovtmagic@gmail.com>
2018-10-20 07:54:31 +0200

    Checking subtasks

--- add.py
+++ add.py
@@ -23,3 +23,17 @@ def add2(self, x, y):
             self.retry(countdown=2)
     sleep(10)
     return x + y
+
+
+@app.task
+def subtask(z):
+    print("SUBTAREA: %s" % z)
+    return 0
+
+
+
+@app.task(bind=True)
+def add_noretry(self, x, y):
+    print("No retry X,Y: %s,%s" % (x,y))
+    subtask.apply_async(args=[x+y])
+    return x + y
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from add import add
+from add import add_noretry
 
 
 #for i in range(0,5):
@@ -6,4 +7,5 @@ from add import add
 
 
 #add.delay(0,3)
-add.apply_async(args=[0,3])
+add_noretry.delay(1,3)
+#add.apply_async(args=[0,3])
